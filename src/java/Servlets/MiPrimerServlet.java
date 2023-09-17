adfdbddd72a663a341a476d02d608601d90b914f
Pablo <Pablo@DESKTOP-ATJP9CE>
2023-09-17 20:18:08 -0300

    Proyecto Servlet para el ramo de  Framework de soluciones empresariales, sección 51. calificada 1.

---
+++ src/java/Servlets/MiPrimerServlet.java
@@ -0,0 +1,104 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package Servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Pablo
+ */
+public class MiPrimerServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        
+        String rut = request.getParameter("rut");
+        String nombre = request.getParameter("nombre");
+        String apellido = request.getParameter("apellido");
+        String direccion = request.getParameter("direccion");
+        String comuna = request.getParameter("comuna");
+        String fechaNacimiento = request.getParameter("fechaNacimiento");
+        String genero = request.getParameter("genero");
+        String redSocial = request.getParameter("social");
+        
+        
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet MiPrimerServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Datos del formulario " + request.getContextPath() + "</h1>" + "<br>");
+            out.println("<h4>El contenido del rut es: " + rut + "</h4>");
+            out.println("<h4>El contenido del nombre es: " + nombre + "</h4>");
+            out.println("<h4>El contenido del apellido es: " + apellido + "</h4>");
+            out.println("<h4>El contenido de la dirección es: " + direccion + "</h4>");
+            out.println("<h4>El contenido de la comuna: " + comuna + "</h4>");
+            out.println("<h4>El contenido de la fecha de nacimiento es: " + fechaNacimiento + "</h4>");
+            out.println("<h4>El contenido del genero es: " + genero + "</h4>");
+            out.println("<h4>El contenido de la red social es: " + redSocial + "</h4>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
